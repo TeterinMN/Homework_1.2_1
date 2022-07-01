986d1f29cb4ad81b47667abce1015da693303e9d
TeterinMN <Scorpion.TMN@ya.ru>
2022-07-01 09:14:45 +0700

    Произведена доработка ДЗ, оптимизация кода

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,18 +5,9 @@
  * За переводы с любых карт комиссия 0.75%, минимум 35 рублей
  */
 
-fun main(){
-    val amount = 108_000_00
-    val amountRub = amount / 100
-    val percentMin = 2_100
-    val percentMinRub = percentMin / 100
-    val result = amount * 0.75 / 100
-    val resultRub = result / 100
-    println()
-    println("Сумма перевода составляет $amount копеек ($amountRub руб.)")
-    if (result <= percentMin) {
-        println("Взята минимальная комиссия  $percentMin копеек ($percentMinRub руб.) ")
-    } else {
-        println("Процент от суммы перевода составил $result копеек ($resultRub руб.)")
-    }
+fun main() {
+    val amount = 12_800_000
+    val percentMin = 3500
+    val result = (amount * 0.75) / 100
+    if (result <= percentMin) println(percentMin) else println(result)
 }
